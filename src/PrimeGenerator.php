a40488ab3ce8c98f2b1b0224cab19a8b7e6d7d9e
Luis Rovirosa <luisrovirosa@gmail.com>
2016-02-08 17:01:27 +0100

    Prime of 1 {} -> nil nil -> constant

---
+++ src/PrimeGenerator.php
@@ -0,0 +1,14 @@
+<?php
+namespace LuisRovirosa;
+
+class PrimeGenerator
+{
+    /**
+     * @param int $number
+     * @return array
+     */
+    public function generate(int $number): array
+    {
+        return [];
+    }
+}
--- tests/PrimeFactorsTest.php
+++ tests/PrimeFactorsTest.php
@@ -1,11 +1,15 @@
 <?php
 
-namespace LuisRovirosa;
+namespace LuisRovirosa\Tests;
 
-class PrimeFactorsTest
+use LuisRovirosa\PrimeGenerator;
+
+class PrimeFactorsTest extends \PHPUnit_Framework_TestCase
 {
     /** @test */
-    public function something()
+    public function the_factors_of_one_is_an_empty_list()
     {
+        $primeGenerator = new PrimeGenerator();
+        $this->assertEquals([], $primeGenerator->generate(1));
     }
 }
