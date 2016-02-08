260dad967e3b181b35d82cebd4687c1ee73e4ab4
Luis Rovirosa <luisrovirosa@gmail.com>
2016-02-08 17:08:28 +0100

    refactor: remove duplication

--- tests/PrimeFactorsTest.php
+++ tests/PrimeFactorsTest.php
@@ -6,18 +6,25 @@ use LuisRovirosa\PrimeGenerator;
 
 class PrimeFactorsTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var  PrimeGenerator */
+    private $primeGenerator;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->primeGenerator = new PrimeGenerator();
+    }
+
     /** @test */
     public function the_factors_of_one_is_an_empty_list()
     {
-        $primeGenerator = new PrimeGenerator();
-        $this->assertEquals([], $primeGenerator->generate(1));
+        $this->assertEquals([], $this->primeGenerator->generate(1));
     }
 
     /** @test */
     public function the_factors_of_a_prime_number_is_the_prime_number()
     {
-        $primeGenerator = new PrimeGenerator();
-        $this->assertEquals([2], $primeGenerator->generate(2));
+        $this->assertEquals([2], $this->primeGenerator->generate(2));
     }
 
 }
